d3d4fc155299a890b62706593a96eef98363a7c3
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-07-27 13:48:59 +0000

    SQL Scripts for alx_book_store db

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Select the columns and their details from INFORMATION_SCHEMA.COLUMNS
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
